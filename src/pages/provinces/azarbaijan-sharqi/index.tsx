03ea766731a1b22798137d8d0dbf0824b86f0e28
Ali Shabani <pyramidalish@gmail.com>
2020-10-24 15:46:29 +0330

    wip

--- src/pages/provinces/azarbaijan-sharqi/index.tsx
+++ src/pages/provinces/azarbaijan-sharqi/index.tsx
@@ -1,5 +1,13 @@
 import * as React from "react"
+import Province from "components/province"
+
+import AzerbaijanSharqiMap from "maps/iran/azarbaijan-sharqi.svg"
+import Provinces from "fixtures/provinces.json"
 
 export default function () {
-  return <p>alborz</p>
+  return (
+    <Province province={Provinces["IR-01"]} SVG={AzerbaijanSharqiMap}>
+      <div>descriptions</div>
+    </Province>
+  )
 }
