08452a092de72158df3b26da486d1bd527ec6832
Hossein Ahmadi <hossein.ahmadi98@outlook.com>
2020-10-27 11:17:39 +0330

    search component completed

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -1,14 +1,18 @@
 import * as React from "react"
 import Provinces from "fixtures/provinces.json"
-import Select from "react-select"
+import Select, { ActionMeta, OptionsType, ValueType } from "react-select"
+import { navigate } from "gatsby"
+import { SearchItem } from "types/SearchItem"
+import { DropdownIndicator } from "react-select/src/components/indicators"
 
 const Search: React.FC = () => {
-  let provinces: any[] = []
-  let cities: any[] = []
+  let provinces: SearchItem[] = []
+  let cities: SearchItem[] = []
   Object.keys(Provinces).map((key: any) => {
     const cityList = Object.keys(Provinces[key].cities).map((cityKey: any) => ({
       label: Provinces[key].cities[cityKey].fa,
       value: Provinces[key].cities[cityKey].en,
+      provinceValue: Provinces[key].en,
     }))
     provinces.push({
       label: Provinces[key].fa,
@@ -26,7 +30,26 @@ const Search: React.FC = () => {
       options: cities,
     },
   ]
-  return <Select defaultValue={undefined} options={groupedOptions} />
+
+  const onSelect = (value: ValueType<any>, action: ActionMeta<SearchItem>) => {
+    if (value && action.action === "select-option") {
+      const url = value.provinceValue
+        ? `/provinces/${value.provinceValue}/cities/${value.value}`
+        : `/provinces/${value.value}`
+      navigate(url)
+    }
+  }
+
+  return (
+    <Select
+      value={null}
+      options={groupedOptions}
+      onChange={onSelect}
+      placeholder="جستجو"
+      isRtl
+      isSearchable
+    />
+  )
 }
 
 export default Search
--- src/types/SearchItem.tsx
+++ src/types/SearchItem.tsx
@@ -1,4 +1,5 @@
 export type SearchItem = {
   value: string
   label: string
+  provinceValue?: string
 }
