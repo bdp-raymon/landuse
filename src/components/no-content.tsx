4a61143f691626ab54d6e073f14095cca4c7f129
Hossein Ahmadi <hossein.ahmadi98@outlook.com>
2020-10-28 17:42:01 +0330

    no content created

--- src/components/no-content.tsx
+++ src/components/no-content.tsx
@@ -1,7 +1,11 @@
 import * as React from "react"
 
 const NoContent: React.FC = () => {
-  return <p>متاسفانه در حال حاضر مطلبی در مورد این شهر موجود نمی باشد.</p>
+  return (
+    <div className="w-full">
+      <p>متاسفانه در حال حاضر مطلبی در مورد این شهر موجود نمی باشد.</p>
+    </div>
+  )
 }
 
 export default NoContent
