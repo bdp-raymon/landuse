3f0ae4cfb832eaebced8e8d8fb171358366d091e
Hossein Ahmadi <hossein.ahmadi98@outlook.com>
2020-10-28 17:43:02 +0330

    azarbayjan bugs fixed

--- src/components/no-content.tsx
+++ src/components/no-content.tsx
@@ -2,7 +2,7 @@ import * as React from "react"
 
 const NoContent: React.FC = () => {
   return (
-    <div className="w-full">
+    <div className="w-full text-center mt-12">
       <p>متاسفانه در حال حاضر مطلبی در مورد این شهر موجود نمی باشد.</p>
     </div>
   )
