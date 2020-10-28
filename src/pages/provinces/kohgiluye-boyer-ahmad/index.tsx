f7b59ca2a131dee77c189329b4281b3f6e39f482
Hossein Ahmadi <hossein.ahmadi98@outlook.com>
2020-10-28 17:00:52 +0330

    kohgluye bug fixed bugs fixed

--- src/pages/provinces/kohgiluye-boyer-ahmad/index.tsx
+++ src/pages/provinces/kohgiluye-boyer-ahmad/index.tsx
@@ -1,5 +1,9 @@
 import * as React from "react"
+import Province from "components/province"
+
+import Map from "maps/iran/kohgiluye-boyer-ahmad.svg"
+import Provinces from "fixtures/provinces.json"
 
 export default function () {
-  return <p>alborz</p>
+  return <Province province={Provinces["IR-23"]} SVG={Map}></Province>
 }
