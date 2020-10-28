ca4128bc68891a390b7ef22c64b5830c2f1ac487
Hossein Ahmadi <hossein.ahmadi98@outlook.com>
2020-10-28 17:10:41 +0330

    hormozgan bugs fixed

--- src/pages/provinces/hormozgan/index.tsx
+++ src/pages/provinces/hormozgan/index.tsx
@@ -1,5 +1,9 @@
 import * as React from "react"
+import Province from "components/province"
+
+import Map from "maps/iran/hormozgan.svg"
+import Provinces from "fixtures/provinces.json"
 
 export default function () {
-  return <p>alborz</p>
+  return <Province province={Provinces["IR-29"]} SVG={Map}></Province>
 }
